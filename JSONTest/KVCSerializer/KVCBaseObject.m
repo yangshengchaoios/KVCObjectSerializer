541a29c0f074134230f1ab8cb2a4210d5d3c7b39
YangShengchao <yangshengchaoios@gmail.com>
2014-04-14 11:23:50 +0800

    为基类添加序列化和反序列化方法

--- JSONTest/KVCSerializer/KVCBaseObject.m
+++ JSONTest/KVCSerializer/KVCBaseObject.m
@@ -20,6 +20,116 @@ const char * property_getTypeString( objc_property_t property );
     return self;
 }
 
+/**
+ *  添加反序列化方法
+ *
+ *  @param aDecoder
+ *
+ *  @return
+ */
+-(id)initWithCoder:(NSCoder *)aDecoder {
+    if (self = [super init]) {
+        unsigned int outCount = 0;
+        objc_property_t *properties = class_copyPropertyList([self class], &outCount);
+        
+        @try {
+            for (int i = 0; i < outCount; i++) {
+                objc_property_t property = properties[i];
+                NSString *key=[[NSString alloc] initWithCString:property_getName(property)
+                                                       encoding:NSUTF8StringEncoding];
+                id value = [aDecoder decodeObjectForKey:key];
+                [self setValue:value forKey:key];
+            }
+        }
+        @catch (NSException *exception) {
+            NSLog(@"Exception: %@", exception);
+            return nil;
+        }
+        @finally {
+            
+        }
+        
+        free(properties);
+    }
+    return self;
+}
+
+/**
+ *  添加序列化方法
+ *
+ *  @param aCoder
+ */
+- (void)encodeWithCoder:(NSCoder *)aCoder {
+    unsigned int outCount = 0;
+    objc_property_t *properties = class_copyPropertyList([self class], &outCount);
+    for (int i = 0; i < outCount; i++) {
+        
+        objc_property_t property = properties[i];
+        NSString *key=[[NSString alloc] initWithCString:property_getName(property)
+                                               encoding:NSUTF8StringEncoding];
+        
+        id value=[self valueForKey:key];
+        if (value && key) {
+            if ([value isKindOfClass:[NSObject class]]) {
+                [aCoder encodeObject:value forKey:key];
+            } else {
+                NSNumber * v = [NSNumber numberWithInt:(int)value];
+                [aCoder encodeObject:v forKey:key];
+            }
+        }
+    }
+    free(properties);
+    properties = NULL;
+    
+    //        NSString *propertyType = [NSString stringWithCString: property_getAttributes(property) encoding:NSUTF8StringEncoding];
+    //        NSLog(@"...... %@", propertyType);
+    //
+    //        id value = [self valueForKey:key];
+    //        if (value==nil) {
+    //            continue;
+    //        }
+    //        if ([propertyType hasPrefix:@"T@"]) {
+    //            //对象
+    ////            NSString * objName = [propertyType substringWithRange:NSMakeRange(3, [propertyType rangeOfString:@","].location-4)];
+    ////            NSLog(@"%@", objName);
+    //            [aCoder encodeObject:value forKey:key];
+    //        } else if([propertyType hasPrefix:@"T{"]) {
+    //            //
+    ////            NSString * objName = [propertyType substringWithRange:NSMakeRange(2, [propertyType rangeOfString:@"="].location-2)];
+    ////            NSLog(@"%@", objName);
+    //            [aCoder encodeObject:value forKey:key];
+    //        } else {
+    //
+    //            [aCoder encodeInt:(int)value forKey:key];
+    //
+    //            //基本类型
+    //            propertyType = [propertyType lowercaseString];
+    //            if ([propertyType hasPrefix:@"ti"]) {
+    //                //int
+    //                [aCoder encodeInt:(int)value forKey:key];
+    //            } else if ([propertyType hasPrefix:@"tf"]) {
+    //                //float
+    //                [aCoder encodeFloat:(NSInteger)value forKey:key];
+    //            } else if([propertyType hasPrefix:@"td"]) {
+    //                //double
+    //                [aCoder encodeFloat:(NSInteger)value forKey:key];
+    //            } else if([propertyType hasPrefix:@"tl"]) {
+    //                //long
+    //                [aCoder encodeFloat:(NSInteger)value forKey:key];
+    //            } else if ([propertyType hasPrefix:@"tc"]) {
+    //                //char
+    //                [aCoder encodeFloat:(NSInteger)value forKey:key];
+    //            } else if([propertyType hasPrefix:@"ts"]) {
+    //                //short
+    //                [aCoder encodeFloat:(NSInteger)value forKey:key];
+    //            } else {
+    //                //NSString
+    //                [aCoder encodeObject:value forKey:key];
+    //            }
+    //        }
+    
+}
+
 - (NSString *) getJsonKeyForPropertyName:(NSString *)propertyName {
     
     return nil;
